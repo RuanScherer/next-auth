4b8bce5256c0117200a6ba3daa7982e699cef763
Ruan Scherer <ruan.vscherer@gmail.com>
2022-09-26 15:53:25 -0300

    feat: create hook for authorization control

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,11 +1,16 @@
 import { useEffect } from "react"
 import { useAuth } from "../contexts/AuthContext"
+import { useCan } from "../hooks/useCan"
 import { setupAPIClient } from "../services/api"
 import { api } from "../services/apiClient"
 import { withSSRAuth } from "../utils/withSSRAuth"
 
 export default function Dashboard() {
   const { user } = useAuth()
+  const userCanSeeMetrics = useCan({
+    permissions: ["metrics.list"],
+    roles: ["administrator", "editor"]
+  })
 
   useEffect(() => {
     api.get("/me")
@@ -17,6 +22,8 @@ export default function Dashboard() {
     <>
       <h1>Dashboard</h1>
       <h2>{user?.email}</h2>
+
+      {userCanSeeMetrics && <div>Métricas</div>}
     </>
   )
 }
